ae45b03aa3e05a9e22812e8ea9477eee3cc4cdf2
Humayon K <humayonkabir007@gmail.com>
2020-03-30 20:02:43 +0600

    edit with redux

--- src/components/posts/EditPost.js
+++ src/components/posts/EditPost.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import PostForm from './PostForm';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 
-const EditPost = ({ editPost, updatedPost, selectedPost }) => {
+const EditPost = ({ editPost, updatedPost, post }) => {
   return (
     <PostForm
       editPost={editPost}
       updatedPost={updatedPost}
-      selectedPost={selectedPost}
+      selectedPost={post}
     />
   );
 };
 
-export default EditPost;
+const mapStateToProps = (state, ownProps) => {
+  const post = state.posts.find(post => post.id === ownProps.match.params.id);
+  return {
+    post
+  };
+};
+
+export default compose(withRouter, connect(mapStateToProps))(EditPost);
